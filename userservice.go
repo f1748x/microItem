d21e9f3fdcb1af1d8dc920601208d480cab2c15f
f1748x <f1748x@163.com>
2021-10-12 10:54:33 +0800

    修改了结构

--- userservice.go
+++ userservice.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"context"
-	user "github.com/f1748x/microItem/protobuf/protobuf"
+
+	user "github.com/f1748x/xquser/protobuf/protobuf"
+	"github.com/micro/go-micro/v2"
 )
 
 type UserService struct {
-
 }
 
-func (u *UserService) GetAccountRequest(ctx context.Context,req *user.GetAccountRequest,res *user.Account)  {
+func (u *UserService) GetAccountRequest(ctx context.Context, req *user.GetAccountRequest, res *user.Account) error {
 
+	res.Id = req.Id
+	res.Username = "小明"
+	res.Address = "杭州市100号"
+	res.Phone = "130000000"
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("go.micro.api.user"),
+	)
+	service.Init()
+	//user.RegisterUserServiceHandler(service.Server(),new(UserService))
+	// 启动服务
+	if err := service.Run(); err != nil {
+		panic(err)
+	}
 }
